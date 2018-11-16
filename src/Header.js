6409b34ae50df2dc7ea63e984526ee5215457e97
lt <logantanous@gmail.com>
2018-11-16 13:28:17 -0600

    add styling to header and add background color to body

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-const HeaderDiv = styled.div``;
+const HeaderDiv = styled.div`
+  box-shadow: 0 2px 0 0 #dadada;
 
-const Company = styled(Link)``;
+  background-color: #ffffff;
+
+  padding-top: 14px;
+  padding-bottom: 14px;
+
+  text-align: center;
+
+  margin-bottom: 30px;
+`;
+
+const Company = styled(Link)`
+  color: #0ab463;
+
+  font-weight: 900;
+
+  font-family: "Montserrat", sans-serif;
+  font-size: 18px;
+  text-decoration: none;
+`;
 
 export const Header = () => (
   <HeaderDiv>
